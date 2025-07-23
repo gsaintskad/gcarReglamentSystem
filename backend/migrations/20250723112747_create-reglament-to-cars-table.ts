d08bf7c26a73911421a9aecec3200ec5fb32515d
Hlib Bezuhlyi <144724842+gsaintskad@users.noreply.github.com>
2025-07-23 14:44:15 +0200

    cars to reglaments migration done

--- backend/migrations/20250723112747_create-reglament-to-cars-table.ts
+++ backend/migrations/20250723112747_create-reglament-to-cars-table.ts
@@ -1,10 +1,21 @@
-import type { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('cars_to_reglaments', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+    table.uuid('reglament_type_id').notNullable();
+    table.uuid('car_id').notNullable();
+    table.integer('mileage_stamp').notNullable();
+    table.boolean('is_active').notNullable().defaultTo(true);
+    table.uuid('auto_park_id').notNullable();
+    table.integer('mileage_deadline').notNullable();
+    table.integer('remember_after_mileage_past').notNullable();
+    table.foreign('reglament_type_id').references('id').inTable('reglament_types').onDelete('CASCADE');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('cars_to_reglaments');
 }
-
