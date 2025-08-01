8bd6d0a6d35d86f3fee99047c8e2747f533c1272
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-08-01 13:55:27 +0200

    :)

--- backend/main.ts
+++ backend/main.ts
@@ -1,5 +1,4 @@
 import { startServer } from "./src/api/api.js";
-import { getActualMyTaxiCarList, getAllAutoParks } from "./src/myTaxi/myTaxi.queries.js";
 import { openMyTaxiSShTunnel } from "./src/myTaxi/myTaxi.ssh.js";
 import reglamentJobs from "./src/reglaments/reglament.bootstrap.js";
 import { devLog } from "./src/shared/dev.utils.js";
@@ -7,7 +6,7 @@ import { devLog } from "./src/shared/dev.utils.js";
 await openMyTaxiSShTunnel;
 
 // console.log(await getActualMyTaxiCarList(new Date('2025-08-01T11:00:00.000Z')));
-// reglamentJobs();
-// startServer();
+reglamentJobs();
+startServer();
 devLog();
 devLog("application running...");
