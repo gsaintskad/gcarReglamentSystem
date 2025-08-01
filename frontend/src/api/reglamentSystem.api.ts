f59122a1f4b578df3e97d0059b732e22f88560c9
Hlib Bezuhlyi <hlib.bezuhlyi9@gmail.com>
2025-08-01 14:42:02 +0200

    auto switching dev/prod via vite

--- frontend/src/api/reglamentSystem.api.ts
+++ frontend/src/api/reglamentSystem.api.ts
@@ -1,6 +1,15 @@
+// Import axios
 import axios from "axios";
 
-const apiUrl = "/api";
+// Access the environment variable
+// In Vite, variables are available on `import.meta.env`
+const apiUrl = import.meta.env.VITE_API_URL;
+
+if (!apiUrl) {
+  console.error("VITE_API_URL is not defined. Please check your .env files.");
+}
+
+// Create an Axios instance with the base URL
 const api = axios.create({
   baseURL: apiUrl,
 });
